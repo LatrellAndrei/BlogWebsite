3493efbb16579e9e9e40d350cb2e57bd15cff289
Latrell Andrei Camurungan <l-c1639@students.uc-bcf.edu.ph>
2024-01-29 15:57:16 +0800

    new files for features and css

--- js/app.js
+++ js/app.js
@@ -22,3 +22,25 @@ const sr = ScrollReveal({
 
 sr.reveal('.banner-content, .newsletter, .blog-header', {delay: 200, origin:'left'})
 sr.reveal('.featured-left, .featured-right, .blog-card', {delay: 200, origin:'top'})
+
+function redirectToFeaturePage(featureId, featurePage) {
+    document.getElementById(featureId).addEventListener('click', function () {
+        // Redirect to another HTML page
+        window.location.href = featurePage;
+    });
+}
+
+redirectToFeaturePage('feature1', 'feature1.html');
+redirectToFeaturePage('feature2', 'feature2.html');
+
+function scrollToFeature() {
+    // Get the position of the feature section
+    const featureSection = document.getElementById('feature-section');
+    const featureSectionPosition = featureSection.offsetTop;
+
+    // Scroll to the feature section
+    window.scrollTo({
+        top: featureSectionPosition,
+        behavior: 'smooth'
+    });
+}
